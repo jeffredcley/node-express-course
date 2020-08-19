a2a0b7cc05db13d09a7a89fe215492e603b3226c
JDL <jlipkowi@gmail.com>
2020-08-19 09:32:13 -0500

    set express server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(8000,function(){
+app.listen(3000,function(){
 console.log("server is running")
 })
 
@@ -17,5 +17,13 @@ app.get('/users', function(req,res){
  	})
 })
 
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 
 app.listen(8000,function(){console.log('server is listening')})
